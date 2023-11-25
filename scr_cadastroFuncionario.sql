f6755e6451f514a84af6abd6924f886afd50fc9d
Kauan Markes <daluzmarkes@gmail.com>
2023-11-25 17:15:41 -0400

    corrigindo bd

--- scr_cadastroFuncionario.sql
+++ scr_cadastroFuncionario.sql
@@ -17,4 +17,25 @@ funcao_Fun varchar(60),
 salario_Fun double
 );
 
+create table Empresas (
+id_emp integer primary key auto_increment, 
+nome_fantasia_emp varchar(200),
+razao_social_emp varchar(200),
+cnpj_emp varchar(30),
+situacao_cadastral_emp varchar(50),
+inicio_atvd_emp varchar(100),
+endereco_emp varchar(300),
+estado_emp varchar(200),
+cidade_emp varchar(100),
+regime_tributario_emp varchar(200),
+tipo_emp varchar(20),
+porte_emp varchar(30),
+telefone_emp varchar(40),
+natureza_juridica_emp varchar(100),
+valor_monetario_emp float,
+nome_proprietario_emp varchar(200),
+cpf_emp varchar(20)
+);
+
 select * from Funcionario;
+select * from Empresas;
